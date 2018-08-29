7bcf3909ba26260ae9c4ba039c8d3e1be3c46628
goraklaudia <gora.klaudia@gmail.com>
2018-08-29 14:48:46 +0200

    First commit

---
+++ src/app/display-posts/display-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-posts',
+  templateUrl: './display-posts.component.html',
+  styleUrls: ['./display-posts.component.css']
+})
+export class DisplayPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
